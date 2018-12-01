4df59eebff678aac4d12419243ba13bf2d802812
lwc <757907818@qq.com>
2018-12-01 12:13:46 +0800

    把本地工程添加到git

---
+++ test/src/com/itheima/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
+    }
+}
